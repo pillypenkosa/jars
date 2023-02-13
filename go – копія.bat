d4b682e1f5eecffad70e2b6f49990cf54287166d
pillypenkosa <pillypenkosa@gmail.com>
2023-02-13 09:26:15 +0200

    Jars DJS

--- go – копія.bat
+++
@@ -1,7 +0,0 @@
-git add .
-git commit -m "Jars DJS"
-git push
-
-
-
-
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const appProjectName = 'Jars';
-const appVersion = '2023-02-13 9:19';
+const appVersion = '2023-02-13 9:25';
 
 
 
