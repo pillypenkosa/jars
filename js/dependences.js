8fb2139b59c9d0ffcbeae5aed1956646c71de153
pillypenkosa <pillypenkosa@gmail.com>
2023-02-25 15:37:38 +0200

    jars DJS 2023-02-21 10:40

--- js/dependences.js
+++ js/dependences.js
@@ -8,12 +8,12 @@ const dependences = {
 
 	listGIT: [ 		// https://pillypenkosa.github.io/database/list/list. ${ k } .js
 
-		'jars',
+		//'jars',
 	],
 
 	listApp: [ 		// list/list. ${ k } .js
 
-		//'jars',
+		'jars',
 	],
 
 };
