f2cd6af2b7dd6d75570f26ac3a04009c181b1e13
pillypenkosa <pillypenkosa@gmail.com>
2026-07-06 17:12:22 +0300

    Jars 6.07.2026 17:10

--- list/list.jars.cat.js
+++ list/list.jars.cat.js
@@ -6,7 +6,7 @@ const arrListJarsVolume = [
 
 	{ id: '_025' 	, title: 'Vol. 0.25' 	, } ,
 	{ id: '_033' 	, title: 'Vol. 0.33' 	, } ,
-	{ id: '_05' 		, title: 'Vol. 0.5' 	, } ,
+	{ id: '_05' 	, title: 'Vol. 0.5' 	, } ,
 	//{ id: '07' 		, title2: 'Vol. 0.7' 	, } ,
 
 ];
@@ -20,17 +20,17 @@ const arrListJarsVolume = [
 const arrListJarsCat = [
 
 
-	{ id: 'wine' 		, title: 'Вино' 				, } ,
-	{ id: 'water' 		, title: 'Вода' 				, } ,
-	{ id: 'energy' 		, title: 'Енергетики' 			, } ,
-	{ id: 'kvass_all' 	, title: 'Квас' 				, } ,
-	{ id: 'coffee' 		, title: 'Кофе' 				, } ,
-	{ id: 'drink' 		, title: 'Напої' 				, } ,
-	{ id: 'beer' 		, title: 'Пиво' 				, } ,
-	{ id: 'beer0' 		, title: 'Пиво безалкогольне' 	, } ,
-	{ id: 'cider' 		, title: 'Сидр' 				, } ,
-	{ id: 'juice' 		, title: 'Сік' 					, } ,
-	{ id: 'tea' 		, title: 'Чай' 					, } ,
+	{ id: 'wine' 		, title: 'Вино' 		, } ,
+	{ id: 'water' 		, title: 'Вода' 		, } ,
+	{ id: 'energy' 		, title: 'Енергетики' 	, } ,
+	{ id: 'kvass_all' 	, title: 'Квас' 		, } ,
+	{ id: 'coffee' 		, title: 'Кофе' 		, } ,
+	{ id: 'drink' 		, title: 'Напої' 		, } ,
+	{ id: 'beer' 		, title: 'Пиво' 		, } ,
+	{ id: 'beer0' 		, title: 'Пиво б/а' 	, } ,
+	{ id: 'cider' 		, title: 'Сидр' 		, } ,
+	{ id: 'juice' 		, title: 'Сік' 			, } ,
+	{ id: 'tea' 		, title: 'Чай' 			, } ,
 
 
 ];
